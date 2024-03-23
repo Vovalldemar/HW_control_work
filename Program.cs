a7d79dcdfce0566752cc6d464d62297ce119539e
VovaNFT <vovalldemar@yandex.ru>
2024-03-23 09:42:55 +0300

    Создал 2 массива input and Output Array  а так же цикл foreach для проверки на длинну строки.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main()
     {
-       
+        string[] inputArray = new string[] { "One", "two", "three", "6" };
+        string[] outputArray = new string[inputArray.Length];
+        int outputIndex = 0;
+
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                outputArray[outputIndex++] = s;
+            }
+        }    
+
         Console.Read();
     }
 }
